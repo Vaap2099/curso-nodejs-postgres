17f5a24a31866cf72921e735958d5225fbfde760
Victor Aaron Alvarez Paredes <89317849+Vaap2099@users.noreply.github.com>
2023-09-26 14:54:53 -0600

    Creacion de tablas con ORM

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -6,7 +6,7 @@ const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
-  const pool = new Pool({ connectionString:URI });
+const pool = new Pool({ connectionString:URI });
 
 
 module.exports = pool;
